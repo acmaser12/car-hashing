d59a6cc6bb442e7ec9d9dfb83f5be7f0cebfe120
acmaser12 <49576923+acmaser12@users.noreply.github.com>
2020-02-22 10:05:45 -0600

    Initial commit

---
+++ src/CarInfo.java
@@ -0,0 +1,35 @@
+/*
+    written by: Adam Maser
+    CSC 450 week 6 project
+    CarInfo.java
+    Class for CarInfo objects. Overrides toString() and hashCode() methods.
+ */
+
+
+public class CarInfo {
+
+    private String make;
+    private String type;
+    private String owner;
+    private String licenseState;
+    private String licensePlate;
+
+    public CarInfo(String make, String type, String owner, String licenseState, String licensePlate) {
+        this.make = make;
+        this.type = type;
+        this.owner = owner;
+        this.licenseState = licenseState;
+        this.licensePlate = licensePlate;
+    }
+
+    @Override
+    public String toString() {
+        return "Car is: " + this.owner + " - " + this.licensePlate + " " + this.licenseState +
+                " " + this.make + " " + this.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.licensePlate.hashCode() + this.licenseState.hashCode() + this.owner.hashCode();
+    }
+}
